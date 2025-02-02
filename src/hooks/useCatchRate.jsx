fec94af3fa9b9827e9c12e03f15bef143017d8ef
Cosmin <cosmintutoveanu00@gmail.com>
2025-02-02 17:49:17 +1100

    new catch algorithm

--- src/hooks/useCatchRate.jsx
+++ src/hooks/useCatchRate.jsx
@@ -49,7 +49,7 @@ const calculateCatchRate = (pkmn_rate, max_hp, current_hp, pokeball, status) => 
     const x = (((max_hp * 3 - current_hp * 2) * 1 * pkmn_rate * pokeball.rate) / (max_hp * 3)) * status.rate
     if (x > 255) return { ball: pokeball.name, hp: current_hp, status: status.name, probabilities: 100 }; // Catch guaranteed
     const y = (65536 / (Math.sqrt(Math.sqrt(255 / x))))
-    const z = (y / 65536) * (y / 65536) * (y / 65536) * 100
+    const z = (y / 65536) * (y / 65536) * (y / 65536) * (y / 65536) * 100
     return { ball: pokeball.name, hp: current_hp, status: status.name, probabilities: Math.round(z * 10) / 10 };
 }
 
