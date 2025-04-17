ab05126adadaf83fbecfc2483fdc439aa63f267d
Cosmin <cosmintutoveanu00@gmail.com>
2025-04-17 23:22:44 +1000

    highlight true

--- src/components/Tools/ToolsListing.jsx
+++ src/components/Tools/ToolsListing.jsx
@@ -4,6 +4,7 @@ import { Image } from 'react-bootstrap'
 import { CatchCalcIcon, SearchIcon, ArchetypeIcon, BerryIcon, CosmeticsIcon, EggIcon, EggMovesIcon, PokedexIcon, BreedingIcon } from '../../assets/icons'
 import { useTranslations } from '../../context/TranslationsContext'
 import { useDarkMode } from '../../context/DarkModeContext'
+import { Badge } from '../Atoms'
 import './module.css'
 
 
@@ -30,13 +31,15 @@ const TOOLS = [
         label: 'Pokemon Search',
         url: '/tools/pokemon-search/',
         icon: SearchIcon,
-        description: 'Search pokemon by ability and move combinations. For team-building or theory-crafting.'
+        description: 'Search pokemon by ability and move combinations. For team-building or theory-crafting.',
+        highlight: true
     },
     {
         label: 'Catch Calculator',
         url: '/tools/catch-calculator/',
         icon: CatchCalcIcon,
-        description: 'Calculate the catch chance of pokemon, depending on the pokeball, status condition, and health.'
+        description: 'Calculate the catch chance of pokemon, depending on the pokeball, status condition, and health.',
+        highlight: true
     },
     {
         label: 'Breeding Simulator',
@@ -68,12 +71,17 @@ export const ToolsListing = () => {
     return (
         <div className={`container ${isDark ? 'dark-mode' : ''}`}> {/* Apply dark mode class if true */}
             <div className="row g-2">
-                {TOOLS.map(({ label, url, icon, description }) => (
+                {TOOLS.map(({ label, url, icon, description, highlight }) => (
                     <div className="col-12 col-sm-6 col-md-4" key={label}>
                         <Link to={url} className={`tool-card ${isDark ? 'dark-mode' : ''}`}>
-                            <div className="tool-card-header">
+                            <div className="tool-card-header d-flex align-items-center">
                                 <Image src={icon} className="tool-icon" />
-                                <h5 className="tool-title">{t(label)}</h5>
+                                <h5 className="tool-title mb-0">{t(label)}</h5>
+                                {highlight && (
+                                    <Badge className="ms-auto" style={{ fontSize: '.6rem', color: 'black' }} bg="warning" pill>
+                                        {t('new')}
+                                    </Badge>
+                                )}
                             </div>
                             <div className="tool-card-body">
                                 <p>{t(description)}</p>
